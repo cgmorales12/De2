eecccd15f8b4f256c9f5eb46f7f24aadfbaad9ab
cgmorales12 <cgmorales1280@gmail.com>
2025-09-19 16:02:05 -0500

    Remove diagnostics screen

--- GestionEmpleadosFrontend/src/app/app.routes.ts
+++ GestionEmpleadosFrontend/src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { Routes } from '@angular/router';
 import { DepartamentosComponent } from './departamentos/departamentos.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { AsignacionesComponent } from './asignaciones/asignaciones.component';
-import { TestConnectionComponent } from './test-connection/test-connection';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'departamentos' },
   { path: 'departamentos', component: DepartamentosComponent },
   { path: 'empleados', component: EmpleadosComponent },
   { path: 'asignaciones', component: AsignacionesComponent },
-  { path: 'diagnostico', component: TestConnectionComponent },
   { path: '**', redirectTo: 'departamentos' }
 ];
